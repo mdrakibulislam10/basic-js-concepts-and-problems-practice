d86d2094c818862115d84c6181eac5895c2f698f
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-02-01 23:05:30 +0600

    delete-obj-prop of an array-of-obj-and-string-prop

--- delete-prop.js
+++ delete-prop.js
@@ -1,10 +1,14 @@
 const deleteProperty = (arr) => {
     let obj = arr[0];
     let prop = arr[1];
-    if (prop in obj) {
+    const firstProp = Object.keys(obj)[0];
+    if (firstProp === prop) {
+        // if (prop in obj) {
         delete obj[prop];
         return "YES";
-    } else {
+    }
+
+    else {
         return "NO";
     }
 }
